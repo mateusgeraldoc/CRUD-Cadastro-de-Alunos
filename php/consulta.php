9395701961794e3af1fe7487187dfb0f8166668e
mateusgeraldoc <mateusgeraldocunha@gmail.com>
2025-02-16 21:25:47 -0300

    consult system completed and button to uptade and delete registers created

--- php/consulta.php
+++ php/consulta.php
@@ -4,18 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Consulta</title>
-    <link rel="stylesheet" href="../css/cadastro.css">
+    <link rel="stylesheet" href="../css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
 </head>
 <body>
     <div class="container">
-        <form action="cadastro.php">
+        <form action="exibir_consulta.php" method="post" >
             <a href="../index.html"><img width="30px" style="opacity: 30%;" src="../img/seta-voltar.png"></a>
             <h1>Consulta</h1>
             <div class="input-container">
-                <select name="turma">
+                <select name="Cod_Turma">
                     <option value="" disabled selected>Turma...</option>
                     <?php
                     ini_set('display_errors', 1);
@@ -23,13 +23,11 @@
                     error_reporting(E_ALL);
 
                     include_once('conexao.php');
-
                     if (!$conexao) {
                         die("Falha na conexão: " . mysqli_connect_error());
                     }
 
                     $consulta=mysqli_query($conexao, "SELECT Cod_Turma as codigo, Nome_Turma as turma FROM Turma");
-
                     if (!$consulta) {
                         die("Erro na consulta: " . mysqli_error($conexao));
                     }
--- php/exibir_consulta.php
+++ php/exibir_consulta.php
@@ -4,13 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Consulta</title>
-    <link rel="stylesheet" href="../css/cadastro.css">
+    <link rel="stylesheet" href="../css/exibir_consulta.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
 </head>
 <body>
     <div class="container">
+    <a href="consulta.php"><img width="30px" style="opacity: 30%;" src="../img/seta-voltar.png"></a>
+        <div class="table-container">
         <table>
             <?php
             include_once('conexao.php');
@@ -26,20 +28,34 @@
                                              FROM Aluno
                                              WHERE Cod_turma = $Cod_Turma");
 
+            $turma = mysqli_query($conexao, "SELECT Turma.Nome_turma as Turma
+                                             FROM Turma
+                                             WHERE Cod_turma = $Cod_Turma");
+
+            if ($turma) {
+             $linha_turma = mysqli_fetch_assoc($turma);
+                echo "<h1>" . $linha_turma['Turma'] . "</h1>";
+            } else {
+                echo "<p>Erro na consulta de Turma: " . mysqli_error($conexao) . "</p>";
+            }
+            
             if ($busca) {
-                while($linha = mysqli_fetch_array($busca)){
-                    echo 
-                    "<tr>
-                        <th>Código</th>
+                echo "<tr>
+                        <th>ID</th>
                         <th>Aluno</th>
                         <th>Telefone</th>
                         <th>Email</th>
-                    </tr>
-                    <tr>
+                    </tr>";
+                    
+                while($linha = mysqli_fetch_array($busca)){
+                    echo 
+                    "<tr>
                         <td>" .$linha['Codigo']. "</td>
                         <td>" .$linha['Aluno']. "</td>
                         <td>" .$linha['Telefone']. "</td>
                         <td>" .$linha['Email']. "</td>
+                        <td> <a class='alterar' href='#'>ALTERAR</a> </td>
+                        <td> <a class='excluir' href='#'>EXCLUIR</a> </td>
                     </tr>";
                 }
             } else {
@@ -47,7 +63,7 @@
             }
             ?>
         </table>
-        <a href="../index.html">Voltar</a>
+        </div>
     </div>
 </body>
 </html>
